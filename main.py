c456e7d9c0ec532958e9ec6eb49cd43287d5218b
jdagdelen <jdagdelen@lbl.gov>
2017-10-10 13:58:31 -0700

    switching main page to nabstracts message

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ import json
 import os
 
 from flask import Flask
-# from flask_bootstrap import Bootstrap
 from pymongo import MongoClient
-
-
-# def create_app():
-#   app = Flask(__name__)
-#   Bootstrap(app)
-
-#   return app
 
 application = Flask(__name__)
 
@@ -23,10 +15,10 @@ mongo_client = MongoClient(
     connect=False)
 db = mongo_client[db_creds["db"]]
 
-# @application.route("/")
-# def hello():
-#     return "Hello World! This is another a test of pushing."
-
 @application.route("/")
+def hello():
+    return "Hello World! This is another a test of pushing."
+
+@application.route("/nabstracts")
 def nabstracts():
     return "{} abstracts".format(db.abstracts.count())
