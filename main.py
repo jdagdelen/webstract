134d5d448e320b59386d350f1b843b21b0fea3f7
jdagdelen <jdagdelen@lbl.gov>
2017-10-10 12:40:12 -0700

    testing pushing updates

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! This is a test of pushing."
 
 def get_number_of_abstracts():
     # do stuff
