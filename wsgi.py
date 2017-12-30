73f5725245d0b1e9eb2374d0f2642728b0ff2167
jdagdelen <jdagdelen@lbl.gov>
2017-12-30 05:41:10 -0700

    no title

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+from web.app import dashapp
+
+# dashapp.css.append_css({
+#     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+if __name__ == "__main__":
+    dashapp.run_server()
